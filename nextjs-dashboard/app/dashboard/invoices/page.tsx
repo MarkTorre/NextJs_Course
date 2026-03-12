6b3c0b4410dcfd5c4fca7d873689d86f7d20c6a7
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-03-12 22:03:51 +0100

    bug(chapter9) add timeout to invoice to show the bug with displaying layout.tsx dashboard skeleton component.

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,3 +1,6 @@
-export default function Page(){
+
+export default async  function Page(){
+   console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return <p>Invoices Page</p>
 }
